15991a52a8a79ac054d4d91317ac856a2f218ac2
Marcosfonseca21 <89661871+Marcosfonseca21@users.noreply.github.com>
2021-08-27 17:29:20 -0300

    Add files via upload

---
+++ PRIMEIROPANO.java
@@ -0,0 +1,13 @@
+
+public class PRIMEIROPANO {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 6;
+		int c = 8;
+		
+		System.out.println("Hello World! " + (a+b*c));
+	}
+
+}
